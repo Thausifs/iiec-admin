fc0b0d277956b192766a6aef7cad258a1b1dd9cf
Thausifs <thausifullah@gmail.com>
2023-01-02 17:23:23 +0530

    url changed

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 import "../styles/header.css";
 import IIEC_logo from "../asserts/images/IIEC_logo.png";
 import { BiLogOut } from "react-icons/bi";
-// import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function Header() {
-  const logout = () => { 
+  const logout = () => {
     console.log("hi");
     localStorage.clear();
     window.location.reload();
-    //  return <Navigate to="/login" />;
-  }
+    return <Navigate to="/" />;
+  };
   return (
     <div className="header_div">
       <div>
         <img src={IIEC_logo} alt=""></img>
       </div>
-      <div className='headerplhlddiv'>
+      <div className="headerplhlddiv">
         <input className="search_pd" placeholder="Search"></input>
         <BiLogOut onClick={() => logout()} className="logouticon" />
         <p>logout</p>
@@ -25,4 +25,4 @@ function Header() {
   );
 }
 
-export default Header
+export default Header;
